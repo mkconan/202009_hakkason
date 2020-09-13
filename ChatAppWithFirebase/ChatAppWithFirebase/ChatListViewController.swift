cd1a3a11d03dc406d53c8d7a2fa9fc6537b9afbb
杉江孝士 <zako0808@gmail.com>
2020-09-14 00:09:56 +0900

    yu commit
    yu commit content
    
    Co-Authored-By: y.mikami <61727104+yu-game@users.noreply.github.com>

--- ChatAppWithFirebase/ChatAppWithFirebase/ChatListViewController.swift
+++ ChatAppWithFirebase/ChatAppWithFirebase/ChatListViewController.swift
@@ -27,6 +27,7 @@ class ChatlListTableViewCell: UITableViewCell{
         super.awakeFromNib()
     }
     
+    
     override func setSelected(_ selected: Bool,animated:Bool){
         super.setSelected(selected, animated:animated)
     }
