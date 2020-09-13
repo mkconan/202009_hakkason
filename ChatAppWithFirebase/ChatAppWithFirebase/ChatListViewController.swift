2d596c55e693b43326c7572e53576f257f8f8c28
杉江孝士 <zako0808@gmail.com>
2020-09-14 00:07:57 +0900

    Update ChatListViewController.swift
    Co-Authored-By: Koutarou electro <49329713+mkconan@users.noreply.github.com>

--- ChatAppWithFirebase/ChatAppWithFirebase/ChatListViewController.swift
+++ ChatAppWithFirebase/ChatAppWithFirebase/ChatListViewController.swift
@@ -26,6 +26,7 @@ class ChatlListTableViewCell: UITableViewCell{
     override func awakeFromNib() {
         super.awakeFromNib()
     }
+    
     override func setSelected(_ selected: Bool,animated:Bool){
         super.setSelected(selected, animated:animated)
     }
